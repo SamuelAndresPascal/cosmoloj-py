d2d94b040b941be188706249065ca26c9f1488d8
Samuel Andrés <samuelandres@yandex.com>
2023-06-28 23:35:37 +0200

    feat(learning): update mutable/frozen tuto

--- learning/mutable/mutable2.py
+++ learning/mutable/mutable2.py
@@ -1,8 +1,6 @@
-from dataclasses import dataclass
+from learning.mutable.model import MutableNotHashable, MutableHashable, ImmutableHashable
 
-from learning.model import MutableNotHashable, MutableHashable, ImmutableHashable
-
-"""Problème : utiliser un objet comme clef de hashage et le retrouver.
+"""Problème : ranger un objet dans un ensemble et le retrouver.
 1. Pour le ranger dans l'ensemble il doit être hashable.
 2. Pour être certain de le retrouver, le hash ne doit pas changer. La recette classique est l'immuabilité (frozen)
 
@@ -16,8 +14,8 @@ def mutable_not_hashable():
     # On crée un mutable non hashable
     object1 = MutableNotHashable(id=1)
 
-    # on place l'objet dans un hashset
-    collection = {object1}  # ==> ERREUR : l'objet n'est pas hashable
+    # on place l'objet dans un hashmap
+    collection = {object1 : 'Trouvé'}  # ==> ERREUR : l'objet n'est pas hashable
 
     print(object1 in collection)
 
@@ -31,17 +29,17 @@ def mutable_hashable():
     # On crée un mutable hashable
     object1 = MutableHashable(id=1)
 
-    # on place l'objet dans un hashset
-    collection = {object1}
+    # on place l'objet dans un hashmap
+    collection = {object1 : 'Trouvé'}
 
-    # on cherche l'objet dans le hashset
-    print(object1 in collection)
+    # on cherche l'objet dans le hashmap
+    print(collection[object1])
 
     # on change l'état de l'objet : on peut car il est mutable
     object1.id = 2
 
-    # on cherche l'objet dans le hashset
-    print(object1 in collection)  # ==> PROBLÈME : on ne le trouve plus !!!
+    # on cherche l'objet dans le hashmap
+    print(collection[object1])  # ==> PROBLÈME : on ne le trouve plus ==> ERREUR python (difficile à comprendre) !!!
 
 
 def immutable_hashable():
@@ -49,17 +47,17 @@ def immutable_hashable():
     # On crée un mutable hashable
     object1 = ImmutableHashable(id=1)
 
-    # on place l'objet dans un hashset
-    collection = {object1}
+    # on place l'objet dans un hashmap
+    collection = {object1 : 'Trouvé'}
 
-    # on cherche l'objet dans le hashset
-    print(object1 in collection)
+    # on cherche l'objet dans le hashmap
+    print(collection[object1])
 
     # on change l'état de l'objet : on peut car il est mutable
     object1.id = 2  # ==> ERREUR : l'objet est marqué "FROZEN" et on évite ainsi l'utiliser comme hashable modifié
 
-    # on cherche l'objet dans le hashset
-    print(object1 in collection)
+    # on cherche l'objet dans le hashmap
+    print(collection[object1])
 
 
 if __name__ == '__main__':
