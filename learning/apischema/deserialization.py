33f1e384cf1cddd38edd7b102e3ce639a00e1df3
Samuel Andrés <samuelandres@yandex.com>
2024-05-27 22:42:39 +0200

    feat(learning): test apischema

---
+++ learning/apischema/deserialization.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Mapping, Collection
+
+from apischema import deserialize
+
+
+@dataclass(frozen=True)
+class Foo:
+    bar: str
+
+
+foo = Foo("bar")
+print(foo.bar)
+
+assert deserialize(Foo, {"bar": "bar"}) == Foo("bar")
+assert deserialize(Mapping[str, Collection[Foo]], {"key": [{"bar": "42"}]}) == {
+    "key": [Foo("42")]
+}
