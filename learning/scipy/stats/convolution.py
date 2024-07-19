a47281d7bd958569ccbe32b956168e733bd2b222
Samuel Andrés <samuelandres@yandex.com>
2024-07-19 12:32:41 +0200

    style(learning:scipy): clean convolution sample code

--- learning/scipy/stats/convolution.py
+++ learning/scipy/stats/convolution.py
@@ -4,13 +4,11 @@ import scipy.stats as stats
 from scipy import signal
 from scipy.ndimage import shift
 
-delta = 1 / (24 * 60)
+delta = 1 / (24 * 60)  # assuming the target frequency unit to be the day, require one prob per "minute"
 period = 5
 big_grid = np.arange(-50, 50, delta)
 
-#dist1 = stats.uniform(loc=2, scale=3)
-#dist2 = stats.norm(loc=0, scale=0.25)
-dist1 = stats.norm(loc=0, scale=0.5)
+dist1 = stats.norm(scale=0.5)
 
 pmf1 = dist1.pdf(big_grid) * delta
 
@@ -24,15 +22,6 @@ for i in range(1, target_nb):
 for i, _ in enumerate(convolutions):
     convolutions[i] = shift(convolutions[i] / delta, period / delta * i)
 
-
-#plt.plot(big_grid, pdf1, label='distribution 1')
-#plt.plot(big_grid, pdf2, label='distribution 2')
-#plt.plot(big_grid, pdf3, label='distribution 23')
-#plt.plot(big_grid, dist1.pdf(big_grid - period * 0), label='source 1')
-#plt.plot(big_grid, dist1.pdf(big_grid - period * 1), label='source 2')
-#plt.plot(big_grid, dist1.pdf(big_grid - period * 2), label='source 3')
-#plt.plot(big_grid, pdf_conf_2_1, label='2 sachant 1')
-#plt.plot(big_grid, pdf_conf_3_2_1, label='3 sachant 2 sachant 1')
 plt.plot(big_grid, sum(convolutions), label='total')
 plt.legend(loc='best'), plt.suptitle('PDFs')
 plt.show()
