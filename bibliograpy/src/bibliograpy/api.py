683d83202e4b1b5f0674f268b5601166164ee6a2
Samuel Andrés <samuelandres@yandex.com>
2024-10-10 20:49:12 +0200

    feat(bibliograpy): improve api stub

--- bibliograpy/src/bibliograpy/api.py
+++ bibliograpy/src/bibliograpy/api.py
@@ -1,31 +1,60 @@
-"""Bibliograpy API module"""
+"""Bibliograpy API module."""
 from __future__ import annotations
 
-from collections.abc import Callable
 from dataclasses import dataclass
+from typing import Callable
+
 
 @dataclass(frozen=True, repr=False)
 class Reference:
-    """A bibliography reference"""
-    type: Callable
+    """A bibliography reference."""
     key: str
     title: str
-    ref: Reference | None = None
 
-    def __str__(self):
-        return self.key
+    def __repr__(self):
+        return f"{self.title} [{self.key}]"
 
-def institution():
-    """d"""
+@dataclass(frozen=True, repr=False)
+class Institution(Reference):
+    """A reference to an institution."""
 
-def tech_report():
-    """e"""
+@dataclass(frozen=True, repr=False)
+class TechReport(Reference):
+    """A reference to a tech report."""
+    institution: Institution | None
 
+@dataclass(frozen=True)
+class ReferenceBuilder:
+    reference_wrapper: Callable[[list[Reference]], str]
 
-def reference(ref: Reference):
-    """The reference decorator."""
-    def internal(obj):
-        obj.__doc__ += "\n\nBibliography:\n\n" + str(ref)
-        return obj
+    @staticmethod
+    def _default_lambda(refs: list[Reference]) -> str:
+        if len(refs)==1:
+            return f"\n\nBibliography: {refs[0]}\n"
+        else:
+            result = "\n\nBibliography:\n\n"
+            for r in refs:
+                result += f"* {r}\n"
+            return result
 
-    return internal
+    @staticmethod
+    def default():
+        return ReferenceBuilder(reference_wrapper=ReferenceBuilder._default_lambda)
+
+    def __call__(self, *refs):
+        """The reference decorator."""
+
+        def internal(obj):
+            if len(refs) == 1:
+                ref0 = refs[0]
+                if isinstance(ref0, Reference):
+                    obj.__doc__ += self.reference_wrapper([ref0])
+                elif isinstance(ref0, list):
+                    obj.__doc__ += self.reference_wrapper(ref0)
+            else:
+                obj.__doc__ += self.reference_wrapper([*refs])
+            return obj
+
+        return internal
+
+reference = ReferenceBuilder.default()
--- bibliograpy/test/test_api.py
+++ bibliograpy/test/test_api.py
@@ -1,26 +1,62 @@
 """Test module for bibliograpy"""
+import pydoc
 
-from bibliograpy.api import reference, Reference, institution, tech_report
+from bibliograpy.api import reference, Institution, TechReport
 
-IAU = Reference(type=institution,
-                key="iau",
-                title="International Astronomic Union")
+IAU = Institution(key="iau", title="International Astronomic Union")
 
-IAU_2006_B1 = Reference(
-    type=tech_report,
+IAU_2006_B1 = TechReport(
     key="iau_2006_b1",
     title="Adoption of the P03 Precession Theory and Definition of the Ecliptic",
-    ref=IAU)
+    institution=IAU)
 
 def test_dependencies_args_default():
     """test deps command without supplying file"""
 
     @reference(IAU_2006_B1)
-    def titi():
+    def toto():
         """ma doc"""
 
-    assert titi.__doc__ == """ma doc
 
-Bibliography:
+    assert (pydoc.render_doc(toto) ==
+"""Python Library Documentation: function toto in module test_api
 
-iau_2006_b1"""
+t\bto\bot\bto\bo()
+    ma doc
+
+    Bibliography: Adoption of the P03 Precession Theory and Definition of the Ecliptic [iau_2006_b1]
+""")
+
+    @reference([IAU_2006_B1, IAU])
+    def titi():
+        """ma doc avec plusieurs références"""
+
+
+    assert (pydoc.render_doc(titi) ==
+"""Python Library Documentation: function titi in module test_api
+
+t\bti\bit\bti\bi()
+    ma doc avec plusieurs références
+
+    Bibliography:
+
+    * Adoption of the P03 Precession Theory and Definition of the Ecliptic [iau_2006_b1]
+    * International Astronomic Union [iau]
+""")
+
+    @reference(IAU_2006_B1, IAU)
+    def tata():
+        """ma doc avec plusieurs références en varargs"""
+
+
+    assert (pydoc.render_doc(tata) ==
+"""Python Library Documentation: function tata in module test_api
+
+t\bta\bat\bta\ba()
+    ma doc avec plusieurs références en varargs
+
+    Bibliography:
+
+    * Adoption of the P03 Precession Theory and Definition of the Ecliptic [iau_2006_b1]
+    * International Astronomic Union [iau]
+""")
