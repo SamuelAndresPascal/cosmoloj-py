e45c2b553f2900239284f39173fc97328be93413
Samuel Andrés <samuelandres@yandex.com>
2024-10-22 01:07:37 +0200

    chore(*): refactoring gitlab ci

--- clean_conda.sh
+++ clean_conda.sh
@@ -1,16 +1,8 @@
 CONDA_ENV=$1
-PYTHON_VERSION=$2
 
 python --version
 conda --version
 which python
-which conda
 conda init bash
-conda env list
-conda list $CONDA_ENV
-conda create -n $CONDA_ENV python=$PYTHON_VERSION -y
 . ~/.bashrc
-conda activate $CONDA_ENV
-conda list
-python --version
-which python
+conda remove -n $CONDA_ENV --all -y
