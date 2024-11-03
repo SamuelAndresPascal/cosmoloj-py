7ee17cfecc1b153228107e4d17c1d9f98c134a5a
Samuel Andrés <samuelandres@yandex.com>
2024-11-03 02:39:01 +0100

    doc(bibliograpy): more scope examples

--- bibliograpy/demo/preprocess_and_use.sh
+++ bibliograpy/demo/preprocess_and_use.sh
@@ -16,7 +16,12 @@ echo
 echo "bibliograpy proprocessing"
 echo "========================="
 echo
+# with explicit import of the shared API scope
 bibliograpy process cosmoloj.bib -o cosmoloj_bib.py -S 'from bibliograpy.api import SHARED_SCOPE' -s SHARED_SCOPE
+# only use implicitly imported shared API scope
+bibliograpy process cosmoloj.bib -o alt_1_cosmoloj_bib.py -s SHARED_SCOPE
+# define a local bibliograpy scope
+bibliograpy process cosmoloj.bib -o alt_2_cosmoloj_bib.py -S 'SCOPE: dict[str, Reference] = {}' -s SCOPE
 
 # use bibliography
 echo
