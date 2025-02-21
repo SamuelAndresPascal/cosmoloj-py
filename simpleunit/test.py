f3f97d211a2c583a3d59435b3567b2e242e64a18
Samuel Andrés <samuelandres@yandex.com>
2025-02-21 01:55:54 +0100

    tmp2

--- simpleunit/test.py
+++ simpleunit/test.py
@@ -1,4 +1,9 @@
-from setuptools_scm import get_version
+from setuptools_scm import get_version, Configuration, _get_version
 
-version = get_version()
+version = get_version(root='..', relative_to=__file__)
 print(version)
+
+conf = Configuration.from_file('pyproject.toml')
+print(conf)
+maybe_version = _get_version(conf, force_write_version_files=True)
+print(maybe_version)
