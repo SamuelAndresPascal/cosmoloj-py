3c38f39ae87cded3d1992b8f7f0fdfb3a1ffa820
Samuel Andrés <samuelandres@yandex.com>
2025-03-07 00:42:06 +0100

    feat(learning): performance impact of local variable initialisation

--- learning/structure/array.py
+++ learning/structure/array.py
@@ -14,3 +14,85 @@ print(type(a))
 
 b = [[]]
 print(type(b))
+
+def toto_local() -> str:
+    a = ["toto", "tata"]
+    b= a[1] + a[0]
+    return b
+
+A = ["tata", "toto"]
+def toto_global() -> str:
+    b= A[1] + A[0]
+    return b
+
+start = time.time_ns()
+for i in range(1000000):
+    toto_local()
+
+print((time.time_ns() - start) / 10**9)
+
+
+
+start = time.time_ns()
+for i in range(1000000):
+    toto_global()
+
+print((time.time_ns() - start) / 10**9)
+
+start = time.time_ns()
+for i in range(1000000):
+    toto_local()
+
+print((time.time_ns() - start) / 10**9)
+
+
+
+start = time.time_ns()
+for i in range(1000000):
+    toto_global()
+
+print((time.time_ns() - start) / 10**9)
+
+
+class Example:
+
+    def __init__(self, arg: str):
+        self._a = arg
+
+    def a(self) -> str:
+        return self._a
+
+
+def tata_local() -> Example:
+    e = Example("toto")
+    return e
+
+E = Example("tata")
+def tata_global() -> Example:
+    return E
+
+start = time.time_ns()
+for i in range(1000000):
+    tata_local()
+
+print((time.time_ns() - start) / 10**9)
+
+
+start = time.time_ns()
+for i in range(1000000):
+    tata_global()
+
+print((time.time_ns() - start) / 10**9)
+
+start = time.time_ns()
+for i in range(1000000):
+    tata_local()
+
+print((time.time_ns() - start) / 10**9)
+
+
+start = time.time_ns()
+for i in range(1000000):
+    tata_global()
+
+print((time.time_ns() - start) / 10**9)
