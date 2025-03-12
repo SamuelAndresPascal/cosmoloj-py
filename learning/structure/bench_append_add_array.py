67661d652d393db1a3ba4483ab2cf3e1c000431f
Samuel Andrés <samuelandres@yandex.com>
2025-03-12 08:13:56 +0100

    refactor(learning): update benchmarks

--- learning/structure/bench_append_add_array.py
+++ learning/structure/bench_append_add_array.py
@@ -3,13 +3,13 @@ import time
 a = []
 start = time.time_ns()
 for i in range(1000000):
-    a.append(i)
+    a += [i]
 
 print((time.time_ns() - start) / 10**9)
 
 a = []
 start = time.time_ns()
 for i in range(1000000):
-    a += [i]
+    a.append(i)
 
 print((time.time_ns() - start) / 10**9)
--- learning/structure/bench_local_global_array.py
+++ learning/structure/bench_local_global_array.py
@@ -8,26 +8,18 @@ import time
 # cas d'un tableau de str
 ##########################
 
-def toto_local() -> str:
-    a = ["toto", "tata"]
-    b = a[1] + a[0]
-    return b
-
 A = ["tata", "toto"]
-def toto_global() -> str:
-    b= A[1] + A[0]
-    return b
-
 
 start = time.time_ns()
 for i in range(1000000):
-    toto_local()
+    a = ["toto", "tata"]
+    b = a[1] + a[0]
 
 print((time.time_ns() - start) / 10**9)
 
 start = time.time_ns()
 for i in range(1000000):
-    toto_global()
+    b= A[1] + A[0]
 
 print((time.time_ns() - start) / 10**9)
 
@@ -45,24 +37,19 @@ class Example:
         return self._a
 
 
-def tata_local() -> Example:
-    e = Example("toto")
-    return e
-
 E = Example("tata")
-def tata_global() -> Example:
-    return E
 
 start = time.time_ns()
 for i in range(1000000):
-    tata_local()
+    e = Example("toto")
+    e.a()
 
 print((time.time_ns() - start) / 10**9)
 
 
 start = time.time_ns()
 for i in range(1000000):
-    tata_global()
+    E.a()
 
 print((time.time_ns() - start) / 10**9)
 
