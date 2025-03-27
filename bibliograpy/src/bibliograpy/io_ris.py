a907c33081c63418ab73417ac33b07dd3c71e91a
Samuel Andrés <samuelandres@yandex.com>
2025-03-27 11:44:36 +0100

    style(*): fix style issues

--- bibliograpy/src/bibliograpy/io_ris.py
+++ bibliograpy/src/bibliograpy/io_ris.py
@@ -2,15 +2,10 @@
 
 import json
 
-from typing import Any, TextIO
+from typing import TextIO
 
-import bibtexparser
 import yaml
-from astroid.brain.brain_dataclasses import FIELD_NAME
-from bibtexparser.bibdatabase import BibDatabase
-from bibtexparser.bwriter import BibTexWriter
 
-from bibliograpy.api_bibtex import TYPES, Reference
 from bibliograpy.api_ris2001 import read_ris_entries, Tags, TypeFieldName
 
 
@@ -29,53 +24,58 @@ def read(s, extension: str) -> list[dict]:
 
     raise ValueError(f'unsupported configuration format {extension}')
 
+def _write_py(o: TextIO, content: list[dict]):
+    """Writes the bibliography content in a RIS 2001 python script."""
+    o.write('from bibliograpy.api_ris2001 import *\n')
+    o.write('\n')
+
+    for bib_entry in content:
+        o.write(f'{bib_entry[Tags.ID].upper()} = ')
+        o.write('{')
+        o.write('\n')
+        for e in bib_entry:
+            if e is Tags.TY:
+                o.write(f"  Tags.{e.name}: TypeFieldName.{bib_entry[e]},")
+            else:
+                o.write(f"  Tags.{e.name}: '{bib_entry[e]}',")
+            o.write('\n')
+        o.write('}')
+        o.write('\n')
+
+def _write_ris2001(o: TextIO, content: list[dict]):
+    """Writes the bibliography content in the RIS 2001 format."""
+    for bib_entry in content:
+        o.write(f'{Tags.TY}  - {bib_entry[Tags.TY]}')
+        o.write('\n')
+
+        for tag in bib_entry:
+
+            if tag is Tags.TY:
+                continue
+
+            if tag.value.repeating:
+                for l in bib_entry[tag]:
+                    o.write(f'{tag}  - {l}')
+                    o.write('\n')
+            else:
+                o.write(f'{tag}  - {bib_entry[tag]}')
+                o.write('\n')
+
+        o.write(f'{Tags.ER}  - ')
+        o.write('\n')
+
+
 def write(o: TextIO, extension: str, content: list[dict]):
     """Writes the bibliography in the format specified by the provided extension."""
 
     if extension == 'py':
-
-        scope: dict[str, Any] = {}
-
-        o.write('from bibliograpy.api_ris2001 import *\n')
-        o.write('\n')
-
-        for ref in content:
-            o.write(f'{ref[Tags.ID].upper()} = ')
-            o.write('{')
-            o.write('\n')
-            for e in ref:
-                if e is Tags.TY:
-                    o.write(f"  Tags.{e.name}: TypeFieldName.{ref[e]},")
-                else:
-                    o.write(f"  Tags.{e.name}: '{ref[e]}',")
-                o.write('\n')
-            o.write('}')
-            o.write('\n')
+        _write_py(o, content)
     elif extension in ['yml', 'yaml']:
         yaml.dump([{k.name: (e[k].name if isinstance(e[k], TypeFieldName) else e[k]) for k in e} for e in content],
                   o,
                   sort_keys=False)
     elif extension in ['ris', 'ris2001', 'ris2011']:
-        for bib_entry in content:
-            o.write(f'{Tags.TY}  - {bib_entry[Tags.TY]}')
-            o.write('\n')
-
-            for tag in bib_entry:
-
-                if tag is Tags.TY:
-                    continue
-
-                if tag.value.repeating:
-                    for l in bib_entry[tag]:
-                        o.write(f'{tag}  - {l}')
-                        o.write('\n')
-                else:
-                    o.write(f'{tag}  - {bib_entry[tag]}')
-                    o.write('\n')
-
-            o.write(f'{Tags.ER}  - ')
-            o.write('\n')
-
+        _write_ris2001(o, content)
     elif extension in ['json']:
         json.dump([{k.name: (e[k].name if isinstance(e[k], TypeFieldName) else e[k]) for k in e} for e in content],
                   fp=o,
