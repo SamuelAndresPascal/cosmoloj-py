c70dd814994211209531d53904c2720a4c32217f
Samuel Andrés <samuelandres@yandex.com>
2025-04-19 00:48:41 +0200

    feat(bibliograpy): url endnote support

--- bibliograpy/src/bibliograpy/api_endnote.py
+++ bibliograpy/src/bibliograpy/api_endnote.py
@@ -47,6 +47,9 @@ class Tags(Tag, Enum):
     I = auto()
     """editor"""
 
+    U = auto()
+    """url"""
+
     def endnote_name(self):
         """Gets the endnote format canonical name."""
         return self._name if self._name is not None else self.name
