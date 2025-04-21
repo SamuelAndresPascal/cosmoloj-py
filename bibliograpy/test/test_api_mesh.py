57023264162a16f3f7bf66f9bbf258c305c5f372
Samuel Andrés <samuelandres@yandex.com>
2025-04-21 10:59:58 +0200

    test(bibliograpy): improve pubmed api tests

--- bibliograpy/test/test_api_mesh.py
+++ bibliograpy/test/test_api_mesh.py
@@ -1,7 +1,7 @@
-"""Test module for PubMed api"""
-from bibliograpy.api_pubmed import MeshPublicationType
+"""Test module for Mesh api"""
+from bibliograpy.api_mesh import MeshPublicationType
 
-def test_pubmed_support():
+def test_mesh_type_support():
     """Test PubMed type parsing."""
     assert (MeshPublicationType.parse("Research Support, U.S. Gov't, P.H.S.")
             == MeshPublicationType.RESEARCH_SUPPORT_US_GOVT_PHS)
