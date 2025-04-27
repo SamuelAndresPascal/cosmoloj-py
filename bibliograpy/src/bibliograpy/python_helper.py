90d9840faaafbe1a91a082f24345a34528216f6d
Samuel Andrés <samuelandres@yandex.com>
2025-04-27 22:31:16 +0200

    style(bibliograpy): fix style issues for default python helper implementation

--- bibliograpy/src/bibliograpy/python_helper.py
+++ bibliograpy/src/bibliograpy/python_helper.py
@@ -9,6 +9,8 @@ from bibliograpy.api_pubmed import Tags as Pubmed
 
 
 class DefaultPythonHelper(PythonHelper):
+    """A default PythonHelper implementation supplying a simple behavior for each bibliograpy supported format,
+    each one implemented in a dedicated method"""
 
     def to_symbol(self, fmt: Format, bib_entry):
         if fmt is Formats.ENDNOTE:
@@ -24,6 +26,7 @@ class DefaultPythonHelper(PythonHelper):
         raise ValueError
 
     def endnote(self, bib_entry: dict[Endnote, str | list[str]]):
+        """Endnote symbol generation based on the author tags and the date tag if available."""
         key = ''
         for i in range(min(3, len(bib_entry[Endnote.A]))):
             key += bib_entry[Endnote.A][i]
@@ -35,16 +38,20 @@ class DefaultPythonHelper(PythonHelper):
 
 
     def refer(self, bib_entry: dict[Refer, str | list[str]]):
+        """Refer symbol generation based on the label tag."""
         return bib_entry[Refer.L].replace('.', '_').upper()
 
 
     def ris2001(self, bib_entry: dict[Ris2001, str | list[str] | Ris2001Type]):
+        """Ris 2001 symbol generation based on the id tag."""
         return bib_entry[Ris2001.ID].replace('.', '_').upper()
 
 
     def ris2011(self, bib_entry: dict[Ris2011, str | list[str] | Ris2011Type]):
+        """Ris 2011 symbol generation based on the id tag."""
         return bib_entry[Ris2011.ID].replace('.', '_').upper()
 
 
     def pubmed(self, bib_entry: dict[Pubmed, str | list[str] | MeshPublicationType]):
+        """PubMed symbol generation based on the pmid tag."""
         return bib_entry[Pubmed.PMID].replace('.', '_').upper()
