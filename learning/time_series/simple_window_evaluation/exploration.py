8978118927f167a3e221cf6a478be748da7ab799
Samuel Andrés <samuelandres@yandex.com>
2025-05-03 18:50:16 +0200

    feat(learning): improve and structure ts learning

--- learning/time_series/simple_window_evaluation/exploration.py
+++ learning/time_series/simple_window_evaluation/exploration.py
@@ -1,91 +1,19 @@
-from datetime import timedelta
 from logging import getLogger, config
 from pathlib import Path
-from typing import override
 
 import numpy as np
 import pandas as pd
 import polars as pl
 
 from learning.time_series.simple_window_evaluation.data_random import random_ts_df
-from learning.time_series.simple_window_evaluation.ts_evaluation import TSEvaluation
+from learning.time_series.simple_window_evaluation.obs_val_win_evaluation import ObsValWindowEvaluation
 
 LOG = getLogger(__name__)
 
-class TSEvaluation1(TSEvaluation):
-
-    def __init__(self,
-                 raw_reference_data: pd.DataFrame,
-                 reference_tsid_label: str,
-                 reference_date_label: str,
-                 raw_modelisation_data: pd.DataFrame,
-                 modelisation_tsid_label: str,
-                 modelisation_date_label: str,
-                 obs_inf: timedelta,
-                 obs_sup: timedelta,
-                 val_inf: timedelta,
-                 val_sup: timedelta):
-        self._raw_reference_data = raw_reference_data
-        self._reference_tsid_label = reference_tsid_label
-        self._reference_date_label = reference_date_label
-        self._raw_modelisation_data = raw_modelisation_data
-        self._modelisation_tsid_label = modelisation_tsid_label
-        self._modelisation_date_label = modelisation_date_label
-        self._obs_inf = obs_inf
-        self._obs_sup = obs_sup
-        self._val_inf = val_inf
-        self._val_sup = val_sup
-
-    @override
-    def reference_date_label(self) -> str:
-        return self._reference_date_label
-
-    @override
-    def reference_tsid_label(self) -> str:
-        return self._reference_tsid_label
-
-    @override
-    def modelisation_date_label(self) -> str:
-        return self._modelisation_date_label
-
-    @override
-    def modelisation_tsid_label(self) -> str:
-        return self._modelisation_tsid_label
-
-    @override
-    def reference_observation_window(self) -> (timedelta, timedelta):
-        return self._obs_inf, self._obs_sup
-
-    @override
-    def reference_validation_window(self) -> (timedelta, timedelta):
-        return self._val_inf, self._val_sup
-
-    @staticmethod
-    def from_day_window(raw_reference_data: pd.DataFrame,
-                        reference_tsid_label: str,
-                        reference_date_label: str,
-                        raw_modelisation_data: pd.DataFrame,
-                        modelisation_tsid_label: str,
-                        modelisation_date_label: str,
-                        obs_inf: int, obs_sup: int, val_inf: int, val_sup: int):
-        return TSEvaluation1(raw_reference_data=raw_reference_data,
-                             reference_tsid_label=reference_tsid_label,
-                             reference_date_label=reference_date_label,
-                             raw_modelisation_data=raw_modelisation_data,
-                             modelisation_tsid_label=modelisation_tsid_label,
-                             modelisation_date_label=modelisation_date_label,
-                             obs_inf=timedelta(days=obs_inf),
-                             obs_sup=timedelta(days=obs_sup),
-                             val_inf=timedelta(days=val_inf),
-                             val_sup=timedelta(days=val_sup))
-
-
-
-
 REFERENCE_SEED = 42
 MODELISATION_SEED = 1
-CARD = 100
-EVT = CARD * 30
+CARD = 1000
+EVT = CARD * 10
 
 START = pd.to_datetime('2015-01-01')
 END = pd.to_datetime('2018-01-01')
@@ -98,23 +26,23 @@ if __name__ == '__main__':
     model = random_ts_df(seed=MODELISATION_SEED, ind_card=CARD, evt_card=EVT, ts_start=START, ts_end=END)
 
     print(reference)
-    print(reference[reference['tsid'] == 324])
+    print(reference[reference['tsid'] == 0])
     print(model)
-    print(model[model['tsid'] == 324])
+    print(model[model['tsid'] == 0])
 
-    evaluation = TSEvaluation1.from_day_window(raw_reference_data=reference,
-                                           reference_tsid_label='tsid',
-                                           reference_date_label='date',
-                                           raw_modelisation_data=model,
-                                           modelisation_tsid_label='tsid',
-                                           modelisation_date_label='date',
-                                           obs_inf=30,
-                                           obs_sup=30,
-                                           val_inf=15,
-                                           val_sup=15)
+    evaluation = ObsValWindowEvaluation.from_day_window(raw_reference_data=reference,
+                                                        reference_tsid_label='tsid',
+                                                        reference_date_label='date',
+                                                        raw_modelisation_data=model,
+                                                        modelisation_tsid_label='tsid',
+                                                        modelisation_date_label='date',
+                                                        obs_inf=30,
+                                                        obs_sup=30,
+                                                        val_inf=15,
+                                                        val_sup=15)
 
     obs_val = evaluation.compute(raw_reference=reference, raw_modelisation=model)
     print(obs_val)
     LOG.info("end mapping")
 
-    print(obs_val[obs_val[0] == 324])
+    print(obs_val[obs_val['tsid'] == 0])
---
+++ learning/time_series/simple_window_evaluation/obs_val_win_evaluation.py
@@ -0,0 +1,101 @@
+from datetime import timedelta
+from logging import getLogger, config, DEBUG
+from pathlib import Path
+from typing import override
+
+import numpy as np
+import pandas as pd
+import polars as pl
+
+from learning.time_series.simple_window_evaluation.ts_evaluation import TSEvaluation
+
+LOG = getLogger(__name__)
+
+class ObsValWindowEvaluation(TSEvaluation):
+
+    TRACE = DEBUG - DEBUG // 2
+
+    def __init__(self,
+                 raw_reference_data: pd.DataFrame,
+                 reference_tsid_label: str,
+                 reference_date_label: str,
+                 raw_modelisation_data: pd.DataFrame,
+                 modelisation_tsid_label: str,
+                 modelisation_date_label: str,
+                 obs_inf: timedelta,
+                 obs_sup: timedelta,
+                 val_inf: timedelta,
+                 val_sup: timedelta):
+        self._raw_reference_data = raw_reference_data
+        self._reference_tsid_label = reference_tsid_label
+        self._reference_date_label = reference_date_label
+        self._raw_modelisation_data = raw_modelisation_data
+        self._modelisation_tsid_label = modelisation_tsid_label
+        self._modelisation_date_label = modelisation_date_label
+        self._obs_inf = obs_inf
+        self._obs_sup = obs_sup
+        self._val_inf = val_inf
+        self._val_sup = val_sup
+
+    @override
+    def reference_time_label(self) -> str:
+        return self._reference_date_label
+
+    @override
+    def reference_tsid_label(self) -> str:
+        return self._reference_tsid_label
+
+    @override
+    def modelisation_time_label(self) -> str:
+        return self._modelisation_date_label
+
+    @override
+    def modelisation_tsid_label(self) -> str:
+        return self._modelisation_tsid_label
+
+    def reference_observation_window(self) -> (timedelta, timedelta):
+        return self._obs_inf, self._obs_sup
+
+    def reference_validation_window(self) -> (timedelta, timedelta):
+        return self._val_inf, self._val_sup
+
+    def preprocess_reference(self, data: pd.DataFrame) -> pd.DataFrame:
+        LOG.log(level=ObsValWindowEvaluation.TRACE, msg='compute observation / validation windows')
+        date_col = data[self.reference_time_label()]
+        obs_inf, obs_sup = self.reference_observation_window()
+        val_inf, val_sup = self.reference_validation_window()
+
+        data['obs_inf'] = date_col - obs_inf
+        data['obs_sup'] = date_col + obs_sup
+        data['val_inf'] = date_col - val_inf
+        data['val_sup'] = date_col + val_sup
+        return data
+
+    @override
+    def process_ts(self, reference_data, model_data):
+        result = super().process_ts(reference_data=reference_data, model_dates=model_data)
+        LOG.log(level=ObsValWindowEvaluation.TRACE, msg='compute observed / validated')
+        obs = model_data[model_data.between(reference_data['obs_inf'], reference_data['obs_sup'])]  # 3s
+        val = obs[obs.between(reference_data['val_inf'], reference_data['val_sup'])]  # 3s
+        result['obs'] = len(obs)
+        result['val'] = len(val)
+        return result
+
+    @staticmethod
+    def from_day_window(raw_reference_data: pd.DataFrame,
+                        reference_tsid_label: str,
+                        reference_date_label: str,
+                        raw_modelisation_data: pd.DataFrame,
+                        modelisation_tsid_label: str,
+                        modelisation_date_label: str,
+                        obs_inf: int, obs_sup: int, val_inf: int, val_sup: int):
+        return ObsValWindowEvaluation(raw_reference_data=raw_reference_data,
+                                      reference_tsid_label=reference_tsid_label,
+                                      reference_date_label=reference_date_label,
+                                      raw_modelisation_data=raw_modelisation_data,
+                                      modelisation_tsid_label=modelisation_tsid_label,
+                                      modelisation_date_label=modelisation_date_label,
+                                      obs_inf=timedelta(days=obs_inf),
+                                      obs_sup=timedelta(days=obs_sup),
+                                      val_inf=timedelta(days=val_inf),
+                                      val_sup=timedelta(days=val_sup))
