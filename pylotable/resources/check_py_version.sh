2aaa2a5d74025f7048292ad737e5345925620b36
Samuel Andrés <samuelandres@yandex.com>
2025-05-16 01:45:36 +0200

    fix(pylotable): fix multiversion support

--- pylotable/resources/check_py_version.sh
+++ pylotable/resources/check_py_version.sh
@@ -6,5 +6,5 @@ conda --version
 pip install .
 conda install pytest -y
 pytest
-python demo/group_proprocessor.py
-python demo/merge_proprocessor.py
+python demo/group_preprocessor.py
+python demo/merge_preprocessor.py
