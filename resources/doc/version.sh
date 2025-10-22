6c35f30b16759423e0c5711db8106fd65158a6e8
Samuel Andrés <samuelandres@yandex.com>
2025-10-22 10:10:50 +0200

    chore(*): try to fix ci

--- resources/doc/version.sh
+++ resources/doc/version.sh
@@ -2,6 +2,8 @@ LANG=$1
 
 echo $VERSION
 echo $LANG
+
 echo $'\n**Version '$VERSION'**' >> docs/$LANG/about.md
-cat docs/$LANG/about.md
 echo $'\nDocumentation generated at '$(date -u +"%F %T")' UTC' >> docs/$LANG/about.md
+
+cat docs/$LANG/about.md
