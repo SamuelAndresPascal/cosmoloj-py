6ff1ea3118ea0d339efd662c603e9a52334e1d75
Samuel Andrés <samuelandres@yandex.com>
2025-10-24 00:55:04 +0200

    chore(*): try to factorize job definitions

--- resources/doc/version.sh
+++
@@ -1,9 +0,0 @@
-LANG=$1
-
-echo $VERSION
-echo $LANG
-
-echo $'\n**Version '$VERSION'**' >> docs/$LANG/about.md
-echo $'\nDocumentation generated at '$(date -u +"%F %T")' UTC' >> docs/$LANG/about.md
-
-cat docs/$LANG/about.md
